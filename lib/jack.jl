e5266304128123550f2a137f358713f81da60f29
Elliot Saba <staticfloat@gmail.com>
2023-07-22 13:55:28 -0400

    Add ability to install things in a subprefix

--- lib/jack.jl
+++ lib/jack.jl
@@ -6,28 +6,26 @@ function extract_env_array(prefix::String)
     envname(idx::Int, suffix) = string(prefix, "_", idx, "_", suffix)
 
     idx = 0
+    packages_by_prefix = Dict{String,Vector{PackageSpec}}()
     array = PackageSpec[]
     while haskey(ENV, envname(idx)) || haskey(ENV, envname(idx, "NAME"))
         # We support multiple formats of JLL specification, that map to different `PackageSpec`'s:
 
         # - "Foo_jll"
         # will map to `PackageSpec(name = "Foo_jll")`, so no version constraints at all
+        local pkg
         if haskey(ENV, envname(idx))
-            push!(array,
-                PackageSpec(
-                    name = ENV[envname(idx)],
-                ),
+            pkg =  PackageSpec(
+                name = ENV[envname(idx)],
             )
 
         # - name: "Foo_jll"
         #   version: "v1.4.0"
         # will map to `PackageSpec(name = "Foo_jll", version="v1.4.0")`.
         elseif haskey(ENV, envname(idx, "NAME")) && haskey(ENV, envname(idx, "VERSION"))
-            push!(array,
-                PackageSpec(
-                    name = ENV[envname(idx, "NAME")],
-                    version = VersionNumber(ENV[envname(idx, "VERSION")]),
-                ),
+            pkg = PackageSpec(
+                name = ENV[envname(idx, "NAME")],
+                version = VersionNumber(ENV[envname(idx, "VERSION")]),
             )
 
         # - name: "Foo_jll"
@@ -41,21 +39,26 @@ function extract_env_array(prefix::String)
             else
                 repo = Pkg.Types.GitRepo(source=ENV[envname(idx, "URL")])
             end
-            push!(array,
-                PackageSpec(;
-                    name = ENV[envname(idx, "NAME")],
-                    repo = repo,
-                ),
+            pkg = PackageSpec(;
+                name = ENV[envname(idx, "NAME")],
+                repo = repo,
             )
         end
+
+        subprefix = get(ENV, envname(idx, "SUBPREFIX"), "")
+        if !haskey(packages_by_prefix, subprefix)
+            packages_by_prefix[subprefix] = PackageSpec[]
+        end
+        push!(packages_by_prefix[subprefix], pkg)
+
         idx += 1
     end
-    return array
+    return packages_by_prefix
 end
 
 prefix = get(ENV, "BUILDKITE_PLUGIN_JACK_PREFIX", joinpath(homedir(), "jack"))
 clones_dir = get(ENV, "BUILDKITE_PLUGIN_JACK_GIT_CLONES_DIR", joinpath(prefix, "..", "jack-clones"))
-install = extract_env_array("BUILDKITE_PLUGIN_JACK_INSTALL")
+packages_by_prefix = extract_env_array("BUILDKITE_PLUGIN_JACK_INSTALL")
 strategy = Symbol(get(ENV, "BUILDKITE_PLUGIN_JACK_STRATEGY", "auto"))
 
 # Tell JLLPrefixes to store its git clones in `clones_dir`
@@ -64,11 +67,13 @@ JLLPrefixes.set_git_clones_dir!(clones_dir)
 @assert strategy in [:copy, :hardlink, :symlink, :auto] """
 Invalid `strategy` specified. Valid values are `copy`, `symlink` and `hardlink`."""
 
-mkpath(prefix) # create the directory if it doesn't already exist.
-
 println("--- Collecting Artifacts")
-artifact_paths = collect_artifact_paths(install)
-display(artifact_paths)
+for (subprefix, packages) in packages_by_prefix
+    mkpath(joinpath(prefix, subprefix))
+    @info("Collecting", subprefix, packages)
+    artifact_paths = collect_artifact_paths(packages)
 
-println("--- Deploying artifacts", prefix, strategy)
-deploy_artifact_paths(prefix, artifact_paths; strategy)
+    println("--- Deploying artifacts")
+    @info("Deploying", prefix=joinpath(prefix, subprefix), strategy)
+    deploy_artifact_paths(joinpath(prefix, subprefix), artifact_paths; strategy)
+end
