a5ffb3ed3ba0558ea819ff1f20f03cf96e7966e2
Elliot Saba <staticfloat@gmail.com>
2025-01-30 05:00:10 +0000

    Update Manifests

--- .buildkite/commands/runtests.sh
+++ .buildkite/commands/runtests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 OS=("windows" "linux" "macos")
 TARGET_WORKFLOW="${1}"
+JULIA_VERSIONS=(1.10 1.11 nightly)
 
-for os in ${OS[@]}; do
-    ytt -f "${TARGET_WORKFLOW}" --data-value os="${os}" --data-value arch="x86_64" | buildkite-agent pipeline upload
+for julia_version in ${JULIA_VERSIONS[@]}; do
+    for os in ${OS[@]}; do
+        ytt -f "${TARGET_WORKFLOW}" --data-value os="${os}" --data-value arch="x86_64" --data-value julia_version="${julia_version}" | buildkite-agent pipeline upload
 
-    if [[ $os == "windows" ]]; then
-        continue
-    fi
-    ytt -f "${TARGET_WORKFLOW}" --data-value os="${os}" --data-value arch="aarch64" | buildkite-agent pipeline upload
+        if [[ $os == "windows" ]]; then
+            continue
+        fi
+        ytt -f "${TARGET_WORKFLOW}" --data-value os="${os}" --data-value arch="aarch64" --data-value julia_version="${julia_version}" | buildkite-agent pipeline upload
+    done
 done
